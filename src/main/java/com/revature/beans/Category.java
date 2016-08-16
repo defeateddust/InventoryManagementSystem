536223c399558ab7b02f8e80572094e080746e2c
Chris Olney <christopher.olney867@topper.wku.edu>
2016-08-16 09:29:25 -0400

    pushing changes to master

--- src/main/java/com/revature/beans/Category.java
+++ src/main/java/com/revature/beans/Category.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class Category {
 private int catagoryId;
 @Column(name="CATEGORY_DESCRIPTION",nullable=false)
 private String description;
-@ManyToMany
+@ManyToMany(fetch=FetchType.EAGER)
 @JoinTable(name="PRODUCT_CATEGORIES",joinColumns=@JoinColumn(name="PRODUCT_UPC"),
 inverseJoinColumns=@JoinColumn(name="CATEGORY_ID"))
 private Set<Product> products;
