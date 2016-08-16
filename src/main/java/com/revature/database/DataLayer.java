10a70d4f4764d63463c45013bac9a6d47cc24f86
Chris Olney <christopher.olney867@topper.wku.edu>
2016-08-16 15:27:48 -0400

    database made woot woot

---
+++ src/main/java/com/revature/database/DataLayer.java
@@ -0,0 +1,29 @@
+package com.revature.database;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.beans.State;
+
+public class DataLayer {
+	DAO dao;
+	private Session session;
+	public DataLayer(){
+		session=SessionFactoryManager.getInstance().openSession();
+		dao = new DAO(session);
+	}
+	
+	public void close(){
+		if(session!=null)session.close();
+	}
+	
+	public void createRow(Object obj){
+		Transaction trans = session.beginTransaction();
+		try{
+		dao.insert(obj);
+		trans.commit();
+		}catch(RuntimeException c){
+			trans.rollback();
+		}
+	}
+}
