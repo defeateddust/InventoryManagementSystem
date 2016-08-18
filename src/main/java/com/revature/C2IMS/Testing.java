d7918d987fd272f6e458da1d0af89206caf30d85
Chris Olney <christopher.olney867@topper.wku.edu>
2016-08-18 13:02:40 -0400

    added log4j

--- src/main/java/com/revature/C2IMS/Testing.java
+++ src/main/java/com/revature/C2IMS/Testing.java
@@ -1,10 +1,13 @@
 package com.revature.C2IMS;
 
+import org.apache.log4j.Logger;
+
 import com.revature.beans.ClientType;
 import com.revature.beans.State;
 import com.revature.database.DataLayer;
 
 public class Testing {
+	private static Logger log = Logger.getRootLogger();
 public static void main(String[] args) {
 //	String[] stateArr = new String[] {"Alabama", "Arkansas", "Arizona", "Alaska", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" };
 //	String[] abbreviations = new String[]{"AL","AR","AZ","AK","CA","CO","CT","DE","FL","GA","HI","IA","ID", "IL","IN","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND", "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"};
@@ -17,9 +20,17 @@ public static void main(String[] args) {
 //		state = new State(i,stateArr[i],abbreviations[i]);
 //		dl.createRow(state);
 //	}
-	ClientType ct= new ClientType(1, "Supplier");
-	DataLayer dl = new DataLayer();
-	dl.createRow(ct);
-	
+//	ClientType ct= new ClientType(1, "Supplier");
+//	DataLayer dl = new DataLayer();
+//	dl.createRow(ct);
+//	
+//}
+ 	log.trace("doing a null check");
+	log.debug("Connecting to database");
+    log.info("Hello World");
+    log.warn("Someone is attempting to access unauthorized resource");
+    log.error("exception thrown ");
+    log.fatal("really bad shutting down");
+
 }
 }
